062cc63afecbe5322cf4e752c8365338b8a4cc73
Derecky Costa <dereckycosta@gmail.com>
2022-02-04 20:14:24 -0300

    feat: login screen implemented

--- src/App.tsx
+++ src/App.tsx
@@ -11,20 +11,23 @@ import {
 import { ThemeProvider } from 'styled-components';
 
 import { Splash } from '@screens/Splash';
+import { SignIn } from '@screens/SignIn';
 import theme from './theme';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { AuthProvider } from '@hooks/auth';
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
+
     <ThemeProvider theme={theme}>
-      <SafeAreaView>
-        <StatusBar barStyle='light-content' backgroundColor='transparent' translucent />
-        <ScrollView>
-          <Splash />
-        </ScrollView>
-      </SafeAreaView>
+      <StatusBar barStyle='light-content' backgroundColor='transparent' translucent />
+      <AuthProvider>
+        <SignIn />
+      </AuthProvider>
     </ThemeProvider>
+
   );
 };
 
---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,73 @@
+import React, { ChangeEvent, useState } from 'react';
+import { KeyboardAvoidingView, Platform } from 'react-native';
+
+import { Input } from '@components/Input';
+import { Button } from '@components/Button';
+
+import { useAuth } from '@hooks/auth';
+
+import brandPng from '@assets/brand-img.png';
+
+import {
+  BrandImg,
+  Container,
+  Content,
+  Title,
+  ForgotPasswordButton,
+  ForgotPasswordLabel
+} from './styles';
+
+export function SignIn() {
+  const { signIn, isLogging } = useAuth();
+
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  function handleSignIn() {
+    signIn(email, password);
+  }
+
+
+  return (
+    <Container>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+      >
+        <Content>
+
+          <BrandImg source={brandPng} />
+
+          <Title>Login</Title>
+          <Input
+            onChangeText={setEmail}
+            value={email}
+            placeholder='E-mail'
+            type='secondary'
+            autoCorrect={false}
+            autoCapitalize='none'
+          />
+
+          <Input
+            onChangeText={setPassword}
+            value={password}
+            placeholder='Senha'
+            type='secondary'
+            autoCorrect={false}
+            autoCapitalize='none'
+            secureTextEntry
+          />
+
+          <ForgotPasswordButton>
+            <ForgotPasswordLabel>Esqueci minha senha</ForgotPasswordLabel>
+          </ForgotPasswordButton>
+
+          <Button
+            title='Entrar'
+            onPress={handleSignIn}
+            isLoading={isLogging}
+          />
+        </Content>
+      </KeyboardAvoidingView>
+    </Container>
+  );
+}

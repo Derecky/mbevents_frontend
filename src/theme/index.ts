226462476f1283332418d79b4a5619e9fd20e2d8
Derecky Costa <dereckycosta@gmail.com>
2022-02-03 21:46:34 -0300

    build: cleaned base project

---
+++ src/theme/index.ts
@@ -0,0 +1,41 @@
+export default {
+  COLORS: {
+    WHITE: '#FFFFFF',
+    GRAY_050: '#FAFAFA',
+    GRAY_100: '#F5F5F5',
+    GRAY_200: '#EEEEEE',
+    GRAY_300: '#E0E0E0',
+    GRAY_400: '#BDBDBD',
+    GRAY_500: '#9E9E9E',
+    GRAY_600: '#757575',
+    GRAY_700: '#616161',
+    GRAY_800: '#424242',
+    GRAY_900: '#212121',
+    BLACK: '#000000',
+
+    BRAND_PRIMARY_LIGHT: '#F2DB7A',
+    BRAND_PRIMARY: '#EBC733',
+    BRAND_PRIMARY_DARK: '#C8A92B',
+
+    BRAND_SECONDARY_LIGHT: '#9C8EC1',
+    BRAND_SECONDARY: '#67519F',
+    BRAND_SECONDARY_DARK: '#584587',
+
+    ALERT_LIGHT: '#FF6666',
+    ALERT: '#FF0000',
+    ALERT_DARK: '#B20000',
+
+    CHECK_LIGHT: '#66CF97',
+    CHECK: '#00AF51',
+    CHECK_DARK: '#007A39',
+
+    GRADIENT_1: ['#594688', '#7564A0'],
+
+  },
+
+  FONTS: {
+    ROBOTO_BOLD: 'Roboto-Bold',
+    ROBOTO_REGULAR: 'Roboto-Regular',
+    DM_SERIF_DISPLAY_REGULAR: 'DMSerifDisplay-Regular'
+  },
+};
